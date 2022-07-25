84789c1412fc9b77f703026f9c996801d108a69b
JelenaJN <onlymonopoly@gmail.com>
2022-07-25 16:34:28 +0300

    OperatorActivity

---
+++ src/com/bootcampexcercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,10 @@
+package com.bootcampexcercise.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String args[]){
+    int i = 8;
+    int j = 5;
+    int result = i-j;
+        System.out.println("Difference = " + result);
+    }
+}
