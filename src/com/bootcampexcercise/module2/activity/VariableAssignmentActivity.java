e49df50cdeb8f0aa29e024566a08a14195461b7a
JelenaJN <onlymonopoly@gmail.com>
2022-07-25 15:59:55 +0300

    Variable

--- src/com/bootcampexcercise/module2/activity/VariableAssignmentActivity.java
+++ src/com/bootcampexcercise/module2/activity/VariableAssignmentActivity.java
@@ -1,11 +1,14 @@
-public class Test {
+package com.bootcampexcercise.module2.activity;
+
+public class VariableAssignmentActivity {
     public static void main(String args[]){
 
         int a = 5;
-       int b = 5;
+        int b = 5;
         a = 2;
         System.out.println(a);
         System.out.println(b);
 
     }
+
 }
