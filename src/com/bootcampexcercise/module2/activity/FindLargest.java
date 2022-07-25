5abc080223ac856d03412713adc1f16657fcce10
JelenaJN <onlymonopoly@gmail.com>
2022-07-25 17:03:42 +0300

    FindLargest

---
+++ src/com/bootcampexcercise/module2/activity/FindLargest.java
@@ -0,0 +1,13 @@
+package com.bootcampexcercise.module2.activity;
+
+public class FindLargest {
+    public static void main(String args[]) {
+        int cat = 5;
+        int dog = 10;
+        if (cat > dog) {
+            System.out.println("cat is bigger than dog");
+        } else {
+            System.out.println("dog is bigger than cat");
+        }
+    }
+}
