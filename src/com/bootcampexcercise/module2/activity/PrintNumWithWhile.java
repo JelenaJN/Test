d5d409a84c9db190d6ef92051b2c131bfacd9deb
JelenaJN <onlymonopoly@gmail.com>
2022-07-26 10:13:42 +0300

    PrintNumWithWhile

---
+++ src/com/bootcampexcercise/module2/activity/PrintNumWithWhile.java
@@ -0,0 +1,20 @@
+package com.bootcampexcercise.module2.activity;
+
+public class PrintNumWithWhile {
+    public static void main(String args[]) {
+
+        int i = 0;
+        while (i < 100) {
+            System.out.println(i);
+            i = i + 2;
+        }
+
+
+        int a = 1;
+        while (a < 100) {
+            System.out.println(a);
+            a = a + 2;
+
+        }
+    }
+}
