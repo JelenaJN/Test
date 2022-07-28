0fafa8a0ba8a664a744ae98751b0e4cffa8f688d
JelenaJN <onlymonopoly@gmail.com>
2022-07-28 12:35:48 +0300

    CustomExceptionActivityTest

---
+++ src/com/bootcampexcercise/module7/activity/CustomExceptionActivityTest.java
@@ -0,0 +1,37 @@
+package com.bootcampexcercise.module7.activity;
+
+public class CustomExceptionActivityTest {
+        public static void main(String[] args) throws CustomExceptionActivity {
+            validateUser("John");
+            validateUser("Mike");
+            validateUser("Jelena");
+        }
+        //1 - Type main method and call validateUser() from it.
+        //Call this method with different names to test it
+
+
+        static void validateUser(String name) throws CustomExceptionActivity {
+            //array of names
+            String []validUsers={"John","Mike","Shanti","Stacie"};
+//flag stores 1 if a match is found else it should remain 0
+            int flag=0;
+            for(int i=0;i<4;i++)
+            {
+
+//2 - Write code to check if parameter name contains a value which is found in validUsers array and change flag's value accordingly
+                if (name == validUsers [i]){
+                    flag=1;
+                }
+            }
+//3 - check if flag is zero, throw CustomExceptionActivity Exception.
+//You may also have to declare this exception in the method call using throws.
+            if (flag==0) {
+                throw new CustomExceptionActivity();
+            }
+
+//4 - else if flag is one, print a message "Welcome to Payroll program".
+            else if(flag==1){
+                System.out.println("Welcome to Payroll program");
+            }
+        }
+    }
