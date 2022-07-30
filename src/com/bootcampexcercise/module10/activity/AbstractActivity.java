7fcd172af0343b5543cb5daea7f08601a18f3db4
JelenaJN <onlymonopoly@gmail.com>
2022-07-30 19:34:22 +0300

    AbstractActivity

---
+++ src/com/bootcampexcercise/module10/activity/AbstractActivity.java
@@ -0,0 +1,11 @@
+package com.bootcampexcercise.module10.activity;
+
+public class AbstractActivity {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.introduce();
+        Students students = new Students();
+        students.introduce();
+    }
+}
